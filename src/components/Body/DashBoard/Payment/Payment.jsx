643818dd138646d7638c73d84c0494606a56a129
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2021-12-26 15:17:26 +0600

    added payment system

--- src/components/Body/DashBoard/Payment/Payment.jsx
+++ src/components/Body/DashBoard/Payment/Payment.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Payment = () => {
     const{id} = useParams();
+    const [order,setOrder] = useState({});
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/orderList/${id}`)
+        .then(res => res.json())
+        .then(data => setOrder(data))
+    },[id]);
 
     return (
         <div>
-            <h1>Pay Please {id}</h1>
+            <h1>Pay Please {order._id}</h1>
         </div>
     );
 };
