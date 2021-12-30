fd26a11a3d82a5664c1ab40beb8bc635026bf22f
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2021-12-30 20:53:25 +0600

    manage services added heroku server api

--- src/components/Body/DashBoard/ManageServices/ManageServices.jsx
+++ src/components/Body/DashBoard/ManageServices/ManageServices.jsx
@@ -18,7 +18,7 @@ const ManageServices = () => {
             confirmButtonText: 'Yes, delete it!'
           }).then((result) => {
             if (result.isConfirmed) {
-              fetch(`http://localhost:5000/services/${id}`,{
+              fetch(` https://floating-cliffs-41974.herokuapp.com/services/${id}`,{
                   method:'DELETE',
                   headers:{
                       "content-type":"application/json"
